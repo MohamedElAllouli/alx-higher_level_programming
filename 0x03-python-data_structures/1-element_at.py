426fbdb47b21e20a69f68333c2bf2323ce0f190e
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2024-01-06 05:09:35 -0800

    task1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
-        exit(None)
+       return None
     if idx > len(my_list):
-        exit(None)
+       return None
     return my_list[idx]
