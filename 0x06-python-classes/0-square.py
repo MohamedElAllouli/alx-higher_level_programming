885f6b7a3edf489d3361a08066a8939b680bdd0a
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2024-01-23 12:44:28 -0800

    task0&1

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a class Square"""
+
+
+class Square:
+    """properties of square.
+    """
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a class Square"""
+
+
+class Square:
+    """properties of square.
+    """
+    def __init__(self, size):
+        self.__size = size
