42bec9805f8c42d89175419cff1ba2d5a14fe3cb
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2024-01-31 03:12:38 -0800

    task0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""empty class Rectangle"""
+
+
+class Rectangle:
+    """
+    Class that defines properties of rectangle
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+mkdir 0x08-python-more_classes
