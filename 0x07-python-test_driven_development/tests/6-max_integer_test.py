7e7c2a25c9dfab4f6475cc20977e57cac400f47b
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2024-01-30 17:40:48 -0800

    tasks

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,7 +5,7 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
-class MaxIntegerTests(unittest.TestCase):
+class TestMaxInteger(unittest.TestCase):
     """Unittest for max_integer([..])"""
 
     def test_is_no_arg(self):
