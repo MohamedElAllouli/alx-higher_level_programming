90dc8ba880ef140bb50c62cbda5e1a3619027e0f
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2024-01-31 09:48:35 -0800

    task8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -120,21 +120,22 @@ class Rectangle:
         type(self).number_of_instances -= 1
 
     def bigger_or_equal(rect_1, rect_2):
-        """ 
+        """
         compare tow rectangle
 
         args:
         rect_1: first rectangle
         rect_2: second reactangle
-        
-        Return: 
+
+        Return:
         the bigger rectangle
         """
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        
+
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
             return rect_1
+
         return rect_2
