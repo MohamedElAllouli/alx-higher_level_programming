64ea308a2049294c5633b190bbc462e198f9f7fd
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2024-02-05 15:29:21 -0800

    task8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """" class Rectangle """
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """ class Rectangle """
     def __int__(self, width, height):
-        super.integer_validator(self, width, "width")
-        super.integer_validator(self, height, "height")
+        super.integer_validator(self, "width", width)
+        super.integer_validator(self, "height", height)
         self.__width = width
         self.__height = height
