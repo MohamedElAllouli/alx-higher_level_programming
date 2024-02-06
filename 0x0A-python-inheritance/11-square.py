f528bb3cff789ecfc7fbe9f7f80205b904215bfd
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2024-02-05 16:19:15 -0800

    task11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""" class Rectangle """
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ class 9-rectangle.py """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """area of square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Method that returns a string"""
+        return "[Square] {:d}/{:d}".format(self__size, self__size)
