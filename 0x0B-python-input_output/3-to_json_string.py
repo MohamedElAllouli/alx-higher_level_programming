2e3096734f72e9103e3bf9cbe1edaabf73ecd471
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2024-02-06 13:40:51 -0800

    task3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ returns the JSON representation of an object (string)"""
+
+
 import json
 
 
 def to_json_string(my_obj):
     """returns the JSON representation of an object (string)"""
-    return json.loads(my_obj)   
+    return json.dumps(my_obj)   
