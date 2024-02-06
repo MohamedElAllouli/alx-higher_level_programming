58546ade9e8226554f1485d50b06d9bba0ec6842
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2024-02-06 15:06:20 -0800

    task

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""writes an Object to a text file,"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """read an Object to a text file,"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        x = son.load(f)
