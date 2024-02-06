4201d7d5eef53f5d2896d61d261563b9e882618a
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2024-02-06 12:38:59 -0800

    task

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" reads a text file """
+
+
+def read_file(filename=""):
+    """reads a text file"""
+    with open("my_file_0.txt", encoding="UTF8") as f:
+        print(f.read())
