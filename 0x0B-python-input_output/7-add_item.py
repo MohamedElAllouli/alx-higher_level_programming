ce5e7eb45285ffd2344be2474b35caeba4dcc9c7
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2024-02-06 15:21:14 -0800

    task

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""script that adds all arguments to a Python list"""
+import json
+import os.path
+import sys
+from sys import argv
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+jsonlist = []
+
+if os.path.exists(filename):
+    json_list = load_from_json_file(filename)
+for index in argv[1:]:
+    jsonlist.append(index)
+
+save_to_json_file(jsonlist, filename)
