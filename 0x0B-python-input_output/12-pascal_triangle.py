40180508d811fbe417a876b8b615b0df8e2bf97f
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2024-02-06 16:36:17 -0800

    task

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-""" function  that returns a list of lists of integers representing the
-            Pascal’s triangle of n"""
-
+""" function  that returns a list of lists of integers"""
 
 
 def pascal_triangle(n):
