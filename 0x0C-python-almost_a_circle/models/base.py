835e3820888cfd14013a537c8dce9f414517c135
MohamedElallouli <alloulialloulimohamed2023@gmail.com>
2024-02-12 18:29:42 -0800

    task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -78,21 +78,24 @@ class Base:
             return []
 
     @classmethod
-    def save_to_file_csv(cls, list_objs):
-        '''object to csv file.'''
+    def load_from_file_csv(cls):
+        '''Loads object to csv file.'''
         from models.rectangle import Rectangle
         from models.square import Square
-        if list_objs is not None:
-            if cls is Rectangle:
-                list_objs = [[o.id, o.width, o.height, o.x, o.y]
-                             for o in list_objs]
-            else:
-                list_objs = [[o.id, o.size, o.x, o.y]
-                             for o in list_objs]
-        with open('{}.csv'.format(cls.__name__), 'w', newline='',
+        ret = []
+        with open('{}.csv'.format(cls.__name__), 'r', newline='',
                   encoding='utf-8') as f:
-            writer = csv.writer(f)
-            writer.writerows(list_objs)
+            reader = csv.reader(f)
+            for row in reader:
+                row = [int(r) for r in row]
+                if cls is Rectangle:
+                    d = {"id": row[0], "width": row[1], "height": row[2],
+                         "x": row[3], "y": row[4]}
+                else:
+                    d = {"id": row[0], "size": row[1],
+                         "x": row[2], "y": row[3]}
+                ret.append(cls.create(**d))
+        return ret
 
     @staticmethod
     def draw(list_rectangles, list_squares):
