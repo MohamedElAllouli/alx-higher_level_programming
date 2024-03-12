04cbbfec6f5fbd3237f7ebbcc1d25caf91dab94b
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2024-03-12 09:08:44 -0700

    task

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
---script that lists all records of the table second_table
---Records should be ordered by score (top first)
+-- script that lists all records of the table second_table
+-- Records should be ordered by score (top first)
 SELECT `score`, `name`
 FROM `second_table`
 ORDER BY `score` DESC;
