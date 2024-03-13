2516c308579636b309445dcbf916f8c329e02364
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2024-03-13 09:08:14 -0700

    task

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,10 @@
+-- Write a script that creates the table id_not_null on your MySQL server.
+-- id_not_null description:
+-- id INT with the default value 1
+-- name VARCHAR(256)
+
+CREATE TABLE IF NOT EXISTS `unique_id`(
+	id INT DEFAULT 1,
+	UNIQUE id,
+	name VARCHAR(256)
+);
