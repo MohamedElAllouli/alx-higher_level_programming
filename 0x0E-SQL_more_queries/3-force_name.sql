7c4079942bde667661446ab465c7120266ca0d05
MohamedElAllouli <alloulialloulimohamed2023@gmail.com>
2024-03-13 08:58:26 -0700

    task

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -2,7 +2,7 @@
 -- force_name description:
 	-- id INT
 	-- name VARCHAR(256) can’t be null
-CREATE TABLE `force_name`(
+CREATE TABLE IF NOT EXISTS `force_name`(
 	id INT;
 	name VARCHAR(256) NOT NULL;
 );
