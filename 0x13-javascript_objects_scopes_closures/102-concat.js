eed45bbbf73b11eac6f2596319c7d5dbdc0eeb24
MohamedElallouli <alloulialloulimohamed2023@gmail.com>
2024-04-11 17:41:44 -0700

    all

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const files = require('files');
-const i = files.readFileSync(process.argv[2], 'utf8');
-const j = files.readFileSync(process.argv[3], 'utf8');
-files.writeFileSync(process.argv[4], i + j);
+const fs = require('fs');
+const a = fs.readFileSync(process.argv[2], 'utf8');
+const b = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], a + b);
