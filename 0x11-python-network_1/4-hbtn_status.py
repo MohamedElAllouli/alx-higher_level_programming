8d161ebd4174941be6ca91512e9180db86fdaa61
MohamedElAllouli <alloulialloulimohamed2023@gamil.com>
2024-04-27 16:44:51 -0700

    task

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""
-Script that takes in a URL, sends a request to the URL and displays
-the value of the X-Request-Id variable found in the header of the response.
-"""
-from sys import argv
-from urllib.request import Request, urlopen
+"""Fetches https://intranet.hbtn.io/status."""
+import requests
 
 
 if __name__ == "__main__":
-    url = argv[1]
-    req = Request(url)
-
-    with urlopen(req) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
