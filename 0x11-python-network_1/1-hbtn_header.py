9f9de9010d84c860685b4e9c9112c23fb3dfebdc
MohamedElAllouli <alloulialloulimohamed2023@gamil.com>
2024-04-27 16:27:08 -0700

    tasks

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""Displays the X-Request-Id header variable of a request to a given URL.
+"""A script that takes in a URL,
 """
+
 import sys
 import urllib.request
 
 
 if __name__ == "__main__":
-    url1 = sys.argv[1]
+    url = sys.argv[1]
 
-    request = urllib.request.Request(url1)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as resp:
+        print(dict(resp.headers).get("X-Request-Id"))
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""Sends a request to a given URL and displays the response body.
+""" Script that takes in a URL and an email, sends a POST request
+ And displays the body of the response
 """
+
 import sys
-import urllib.error
+import urllib.parse
 import urllib.request
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
 
-    request = urllib.request.Request(url)
-    try:
-        with urllib.request.urlopen(request) as response:
-            print(response.read().decode("ascii"))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as resp:
+        print(resp.read().decode("utf-8"))
