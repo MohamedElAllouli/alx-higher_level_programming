5d2136808405c4fdb3e8a2df7a99c8262feb7edf
MohamedElAllouli <alloulialloulimohamed2023@gamil.com>
2024-04-27 16:55:47 -0700

    task

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
-import requests
+
+"""
+get https://alx-intranet.hbtn.io/status using
+requests and print the response to stdout
+"""
+
+from requests import get
 
 
-if __name__ == '__main__':
-    url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
-    text = r.text
+def get_alx_intranet(url='https://alx-intranet.hbtn.io/status'):
+    """
+    Send a GET request to the url
+    and print the reponse
+    """
+    res = get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+    print("\t- type: {}".format(str(res).__class__))
+    print("\t- content: {}".format(res.text))
+
+
+if __name__ == "__main__":
+    get_alx_intranet()
